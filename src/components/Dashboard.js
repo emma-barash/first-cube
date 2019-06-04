95dbb735cbe3be297819a48a885dc4e2e5169ceb
Emma Barash <emma.barash.dev@gmail.com>
2019-06-04 16:16:45 -0600

    created Dash component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 const Dashboard = () => {
     return (
-        <div className="rotating-rb">
-            <div className="single-rb"></div>
+        <div className="rotating-box">
+            <div className="single-rb">
+                <div className="front-side">
+                    <img src="" alt="" />
+                </div>
+                <div className="back-side">
+                    <img src="" alt="" />
+                </div>
+                <div className="left-side">
+                    <img src="" alt="" />
+                </div>
+                <div className="right-side">
+                    <img src="" alt="" />
+                </div>
+                <div className="top-side">
+                    <img src="" alt="" />
+                </div>
+                <div className="bottom-side">
+                    <img src="" alt="" />
+                </div>
+            </div>
         </div>
     );
 };
