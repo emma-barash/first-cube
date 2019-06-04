9c4f54320b111fb3261af22eb42256ca9692be81
Emma Barash <emma.barash.dev@gmail.com>
2019-06-04 16:11:34 -0600

    first commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div className="rotating-rb">
+            <div className="single-rb"></div>
+        </div>
+    );
+};
+
+export default Dashboard;
